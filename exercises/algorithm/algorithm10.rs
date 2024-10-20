3ced13e1caacf3f7dfbcbb76b23bd40e79cc1c28
Testtraveler <liaomoumou0613@outlook.com>
2024-10-20 22:05:18 +0800

    all ok!

--- exercises/algorithm/algorithm10.rs
+++ exercises/algorithm/algorithm10.rs
@@ -29,7 +29,10 @@ impl Graph for UndirectedGraph {
         &self.adjacency_table
     }
     fn add_edge(&mut self, edge: (&str, &str, i32)) {
-        //TODO
+        let (from, to, weight) = edge;
+        // Add the edge to both nodes since it's an undirected graph
+        self.adjacency_table_mutable().entry(String::from(from)).or_insert(Vec::new()).push((String::from(to), weight));
+        self.adjacency_table_mutable().entry(String::from(to)).or_insert(Vec::new()).push((String::from(from), weight));
     }
 }
 pub trait Graph {
@@ -37,11 +40,24 @@ pub trait Graph {
     fn adjacency_table_mutable(&mut self) -> &mut HashMap<String, Vec<(String, i32)>>;
     fn adjacency_table(&self) -> &HashMap<String, Vec<(String, i32)>>;
     fn add_node(&mut self, node: &str) -> bool {
-        //TODO
-		true
+        if self.contains(node) {
+            return false;
+        }
+        self.adjacency_table_mutable().insert(String::from(node), Vec::new());
+        true
     }
     fn add_edge(&mut self, edge: (&str, &str, i32)) {
-        //TODO
+        let (from, to, weight) = edge;
+        if !self.contains(from) {
+            self.add_node(from);
+        }
+        if !self.contains(to) {
+            self.add_node(to);
+        }
+        let from_node_neighbours = self.adjacency_table_mutable().entry(from.to_string()).or_insert(Vec::new());
+        from_node_neighbours.push((to.to_string(), weight));
+        let to_node_neighbours = self.adjacency_table_mutable().entry(to.to_string()).or_insert(Vec::new());
+        to_node_neighbours.push((from.to_string(), weight));
     }
     fn contains(&self, node: &str) -> bool {
         self.adjacency_table().get(node).is_some()
